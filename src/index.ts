425b00fa44a0486eb1993030bcf434dabb7f2a12
AgReSSoR <samahkinets@gmail.com>
2023-09-05 19:01:23 +0300

    Изменил ссылку на сайт, в настройках cors

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 5000 
  
 const corsOptions = {
-    origin: 'http://127.0.0.1:5500', 
+    origin: 'https://umma-e96w.onrender.com', 
 }; 
 
 const __filename = fileURLToPath(import.meta.url);
