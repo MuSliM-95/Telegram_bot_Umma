99cbdd5f053a33de09b190cc4b4c054564fe8ef8
AgReSSoR <samahkinets@gmail.com>
2023-11-14 22:20:37 +0300

    test

--- src/db/routs/rout.ts
+++ src/db/routs/rout.ts
@@ -4,7 +4,7 @@ import upload from "../middleWares/upload.js"
 
 const router = Router()
 
-router.post("/dat
-a",  upload.single("photo"),  addressController.postData) 
+router.post("/data",  upload.single("photo"),  addressController.postData) 
 router.get("/addresses/:jsonLocation",   addressController.getAddresses) 
-export default router  
+
+export default router 
