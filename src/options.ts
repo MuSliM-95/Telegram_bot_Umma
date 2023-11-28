e3636e20ce366156152a68906daa2dd6399b8238
AgReSSoR <samahkinets@gmail.com>
2023-11-28 03:08:17 +0300

    test

--- src/options.ts
+++ src/options.ts
@@ -93,7 +93,7 @@ export const addressInfoAdminChat = async (data: Data, obj: Bot) => {
          
 
     } catch (error) {
-        console.error("Ошибка при отправке фото:", error);
+        console.error("Ошибка при отправке фото:", ( error as Error).message);
     }
 
 }
